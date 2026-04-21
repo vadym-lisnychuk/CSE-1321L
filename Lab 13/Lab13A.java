7a9968d86ac59618382c552a3a136d53dcfa6ec1
Vadym <vadymlisnychuk@icloud.com>
2026-04-21 13:25:44 -0400

    lab 13 complete

--- Lab 13/Lab13A.java
+++ Lab 13/Lab13A.java
@@ -3,25 +3,8 @@ import java.util.Scanner;
 class Lab13A{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-
-        String[] meals = {
-            "Hummus and veggie wrap",
-            "Veggie stir fry with tofu",
-            "Chicken salad wrap",
-            "Grilled chicken with vegetables",
-
-            "Cheese quesadilla",
-            "Mac and cheese",
-            "Ham and Cheese sandwich",
-            "Pasta with meat sauce",
-
-            "Fruit and yogurt bowl",
-            "Tomato soup with bread",
-            "Yogurt with granola and honey",
-            "Egg drop soup with rice"
-        };
-
         System.out.println("[Meal Selector]");
+        
         System.out.print("How many minutes do you have to prep your meal?: ");
         int minutes = scanner.nextInt();
         scanner.nextLine(); 
@@ -33,42 +16,53 @@ class Lab13A{
         System.out.print("Vegetarian option? (true/false): ");
         String vegan = scanner.nextLine();
 
-
         boolean isVegan = (vegan.equals("true"));
-    
-
         boolean isQuick = (minutes <= 15);
 
-        System.out.println(isVegan);
         // 1 - healthy, 2 - comfort, 3 - light snack
-        int index = 0;
+        String meal = "";
         switch (mood) {
             case 1:
+                if(isVegan)
+                    if(isQuick)
+                        meal += "Hummus and veggie wrap";
+                    else
+                        meal += "Veggie stir fry with tofu";
+                else 
+                    if(isQuick)
+                        meal += "Chicken salad wrap";
+                    else
+                        meal += "Grilled chicken with vegetables";
                 break;
             case 2:
-                index += 4;
+                if(isVegan)
+                    if(isQuick)
+                        meal += "Cheese quesadilla";
+                    else
+                        meal += "Mac and cheese";
+                else 
+                    if(isQuick)
+                        meal += "Ham and Cheese sandwich";
+                    else
+                        meal += "Pasta with meat sauce";
                 break;
             case 3:
-                index += 8;
+                if(isVegan)
+                    if(isQuick)
+                        meal += "Fruit and yogurt bowl";
+                    else
+                        meal += "Tomato soup with bread";
+                else 
+                    if(isQuick)
+                        meal += "Yogurt with granola and honey";
+                    else
+                        meal += "Egg drop soup with rice";
                 break;
             default:
                 break;
+        
         }
-
-        if (isVegan){
-            if(isQuick)
-                index += 0;
-            else
-                index += 1;
-        }
-        else {
-            if(isQuick)
-                index += 2;
-            else
-                index += 3;
-        }
-
-        System.out.print("You should try " + meals[index]);
+        System.out.print("You should try " + meal);
         scanner.close();
     }
 }
--- Lab 13/Lab13B.java
+++ Lab 13/Lab13B.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 class Lab13B{
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+        System.out.println("Enter your lab grades:");
+        int n = 13;
+        float current_grade;
+        float min_grade = 0;
+        float sum = 0;
+        for(int i = 0; i < n; i++){
+            System.out.print(String.format("Lab %d: ", i + 1));
+            current_grade = scanner.nextFloat();
+            scanner.nextLine();
+
+            sum += current_grade;
+            if(i == 0)
+                min_grade = current_grade;
+            
+            if(current_grade < min_grade)
+                min_grade = current_grade;
+        }
+        System.out.println(String.format("The lowest lab grade was: %f", min_grade));
+        System.out.println(String.format("Your lab grade average is: %f", sum / 13));
+        System.out.println(String.format("Your lab grade average after policy is : %f", (sum - min_grade)/ 12));
+        System.out.print("Do you want to try again? (Y/N): ");
+        String cont = scanner.nextLine();
+
+        if(cont.equals("N"))
+            break;
+
+        }
+        scanner.close();
+
     }
 }
